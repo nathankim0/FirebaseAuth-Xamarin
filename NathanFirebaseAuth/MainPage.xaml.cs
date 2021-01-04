ca00083c6b9c020e2680f22ef627e25cc4bf1580
Jinyeob <jinyeob07@gmail.com>
2021-01-04 17:22:59 +0900

    init

---
+++ NathanFirebaseAuth/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Firebase.Auth;
+using Newtonsoft.Json;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace NathanFirebaseAuth
+{
+    public partial class MainPage : ContentPage
+    {
+        public string WebAPIkey = "AIzaSyDuUX2cRjUinWLuqPq7cfbePTvNeREJAqM";
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void signupbutton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            try
+            {
+                var authProvider = new FirebaseAuthProvider(new FirebaseConfig(WebAPIkey));
+                var auth = await authProvider.CreateUserWithEmailAndPasswordAsync(UserNewEmail.Text, UserNewPassword.Text);
+
+
+                string gettoken = auth.FirebaseToken;
+                await App.Current.MainPage.DisplayAlert("Alert", gettoken, "Ok");
+            }
+            catch (Exception ex)
+            {
+                await App.Current.MainPage.DisplayAlert("Alert", ex.Message, "OK");
+            }
+
+        }
+
+        async void loginbutton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var authProvider = new FirebaseAuthProvider(new FirebaseConfig(WebAPIkey));
+            try
+            {
+                var auth = await authProvider.SignInWithEmailAndPasswordAsync(UserLoginEmail.Text, UserLoginPassword.Text);
+                var content = await auth.GetFreshAuthAsync();
+                var serializedcontnet = JsonConvert.SerializeObject(content);
+                Preferences.Set("MyFirebaseRefreshToken", serializedcontnet);
+                await Navigation.PushAsync(new MyDashboardPage());
+            }
+            catch (Exception ex)
+            {
+                await App.Current.MainPage.DisplayAlert("Alert", "Invalid useremail or password", "OK");
+            }
+        }
+    }
+}
